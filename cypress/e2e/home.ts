e7c14f3edb2c7d1ee54859c7fcde6e306bff13e6
João Pedro <joaopsilvamagalhaes@gmail.com>
2021-12-24 22:08:21 -0300

    chore: cypress scope

---
+++ cypress/e2e/home.ts
@@ -0,0 +1,12 @@
+/// <reference path="../support/index.d.ts" />
+
+describe('Home Page', () => {
+  it('should render home sections', () => {
+    cy.visit('/')
+
+    cy.get('.slick-slider').within(() => {
+      cy.findByRole('heading', { name: /cyberpunk 2077/i })
+      cy.findByRole('link', { name: /buy now/i })
+    })
+  })
+})
