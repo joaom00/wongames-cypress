3d9b5085608206387854149625c516b390c71f54
João Pedro <joaopsilvamagalhaes@gmail.com>
2022-02-07 23:07:59 -0300

    test(cypress): add shouldRenderBanner command

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -4,9 +4,6 @@ describe('Home Page', () => {
   it('should render home sections', () => {
     cy.visit('/')
 
-    cy.get('.slick-slider').within(() => {
-      cy.findByRole('heading', { name: /cyberpunk 2077/i })
-      cy.findByRole('link', { name: /buy now/i })
-    })
+    cy.shouldRenderBanner()
   })
 })
