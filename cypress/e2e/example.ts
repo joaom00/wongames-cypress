a66b37901ff0d4ca6d83aee54ddb5eda4b777130
João Pedro Magalhães <joaopsilvamagalhaes@gmail.com>
2022-04-09 20:48:41 -0300

    chore(cypress): wishlist

--- cypress/e2e/example.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="../support/index.d.ts" />
-
-describe('Cypress TS', () => {
-  it('should go to go Google', () => {
-    cy.google()
-  })
-})
---
+++ cypress/e2e/wishlist.ts
@@ -0,0 +1,27 @@
+/// <reference path="../support/index.d.ts" />
+
+describe('Wishlist', () => {
+  it('should add and remove games from wishlist', () => {
+    cy.visit('/wishlist')
+
+    cy.signIn()
+
+    cy.findByText(/Your wishlist is empty/i).should('exist')
+
+    cy.getByDataCy('game-card')
+      .first()
+      .within(() => {
+        cy.findAllByLabelText(/add to wishlist/i).click()
+      })
+
+    cy.getByDataCy('wishlist').within(() => {
+      cy.getByDataCy('game-card').should('have.length', 1)
+    })
+
+    cy.getByDataCy('wishlist').within(() => {
+      cy.findAllByLabelText(/remove from wishlist/i).click()
+    })
+
+    cy.findByText(/your wishlist is empty/i).should('exist')
+  })
+})
